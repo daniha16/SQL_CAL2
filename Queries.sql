98fbe5413864bca844b568af89b0e76019c6ae89
Dani Hernández <47524630+daniha16@users.noreply.github.com>
2019-04-12 21:08:53 +0200

    Update Queries.sql

--- Queries.sql
+++ Queries.sql
@@ -15,37 +15,47 @@ where concerts.Country like "Spain";
 select disks.Referencenumber
 	from disks, (select count(distinct has.name) as numsongs , disks.Referencenumber as numero
 		from disks, has where disks.Referencenumber=has.Referencenumber group by has.Referencenumber) as Referencenumber
-			where numsongs>10 and disks=numero;
+			where numsongs>10 and disk=numero;
 select D.Referencenumber
-	from disks as D 
-		where (select count(distinct has.name) from disks as D1 natural join has where D1.Referencenumber=D.Referencenumber)>10;
+	from disk as D 
+		where (select count(distinct has.name) from disk as D1 natural join has where D1.Referencenumber=D.Referencenumber)>10;
 /*EX6*/
 select musicgroup.* from musicgroup, musician, members, 
 	(select count(distinct members.ID) as numero, members.ID as dnis from members group by members.ID) as pp
 where members.id=dnis and musicgroup.groupID=members.groupID and numero>3;
 /*EX7*/
 select opinions.opdescription 
-	from opinions, buy, customer, (select count(distinct buy.ID) as cantidad, buy.Id codigo from buy group by buy.Referencenumber) as pp
-		where cantidad>3 and buy.ID=codigo and opinions.email=user.email and customer.DNI=buy.ID;
+	from opinions, buy, user, (select count(distinct buy.ID) as cantidad, buy.Id codigo from buy group by buy.Referencenumber) as pp
+		where cantidad>3 and buy.ID=codigo and opinions.email=user.email and user.DNI=buy.ID;
 /*EX8*/
---
+!->select disk.title, disk.Referencenumber 
+	from disk inner join has on disk.Referencenumber = has.disk.Referencenumber
+where has.duration>5 and disk.format = "Physical" and disk.typeofphy = "CD";
 /*EX9*/
 select musician.name from musicgroup, perform, musician, concerts, members
 where concerts.City like "Madrid" and perform.Code=concerts.Code and perform.groupID=musicgroup.groupID and
 members.groupID=musicgroup.groupID and members.ID=musician.ID;
 /*EX10*/
---
+!->select opdescription 
+	from aboutconcerts, musicgroup, perform, musician, concerts, members
+where concerts.City like "Madrid" and perform.Code=concerts.Code and perform.groupID=musicgroup.groupID and
+members.groupID=musicgroup.groupID and members.ID=musician.ID;
 /*EX11*/
-select song.*, musician.name from (((musician natural join composes) natural join song) natural join has) natural join disks
-where disks.genre like "Heavy Metal" and disks.year=2018;
+select song.*, musician.name from (((musician natural join composes) natural join song) natural join has) natural join disk
+where disk.genre like "Heavy Metal" and disk.year=2018;
 /*EX12*/
 select musician.name from musician, plays, instrument, musicgroup, members
 where musicgroup.genre like "Jazz" and musician.ID=members.ID and members.ID=musicgroup.groupID
 and plays.ID=musician.ID and instrument.numserie=plays.numserie and instrument like "guitar";
 /*EX13*/
---
+!->select user.name, user.surname
+	from user inner join buytickets on user.email = buytickets.email inner join tickets on tickets.ticketcode = buytickets.ticketcode
+	natural join concerts inner join perform on perform.Code = concerts.code inner join groups on musicgroup.groupID = perform.groupID
+where concerts.year = tickets.year = 2018 and concerts.month = tickets.month and concerts.day = ticekts.day and musicgroup.genre = "Jazz";
 /*EX14*/
---
+!->select disk.title, song.name 
+	from disk inner join has on has.Referencenumber = disk.Referencenumber inner song on song.name = has.name inner join aboutdisks on disks.Referencenumber = disk.Referencenumber
+where aboutdisks.rating>8;
 /*EX15*/
 select musicgroup.*, members.ID from musicgroup inner join members on members.groupID=musicgroup.groupID
 where musicgroup.groupID 
@@ -53,10 +63,10 @@ where musicgroup.groupID
 		from musicgroup, perform, concerts where concerts.Code not in (select concerts.Code from concerts where concerts.Country like "Spain")
 			and concerts.Code=perform.Code and musicgroup.groupID=perform.groupID)
 	and musicgroup.groupID 
-    in(select musicgroup.groupID from musicgroup, record, disks, (select count(has.name) as numsongs, disks.Referencenumber as disc 
-													from has natural join disks group by has.Referencenumber) as pp
+    in(select musicgroup.groupID from musicgroup, record, disk, (select count(has.name) as numsongs, disk.Referencenumber as disc 
+													from has natural join disk group by has.Referencenumber) as pp
 								where record.groupID=musicgroup.groupID 
-									and record.Referencenumber=Disks.Referencenumber and numsongs>10 and disc=disks.Referencenumber);
+									and record.Referencenumber=disk.Referencenumber and numsongs>10 and disc=disks.Referencenumber);
 /*EX16*/
 select song.name, musician.name from musician, song, musicgroup where musician.ID
 not in(select members.ID from members, musicgroup, concerts, perform where concerts.Code 
